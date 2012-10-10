55e29b8a62700a589745bdc84f555778dc7e08f7
chapmanb <chapmanb@50mail.com>
2012-10-10 10:27:22 -0400

    Allow specification of history for uploading to Galaxy

--- src/blend/galaxy/tools.clj
+++ src/blend/galaxy/tools.clj
@@ -6,19 +6,23 @@
 
 (defn run-tool
   "Run a remote tool on Galaxy server"
-  [client tool-id params]
+  [client tool-id params & {:keys [history-id]}]
   (let [clean-params (reduce (fn [coll [k v]]
                                (assoc coll (string/replace (name k) "-" "_") v))
                              {} params)
-        tool-client (.getToolsClient client)]
-    (.create tool-client (histories/get-current-history client)
+        tool-client (.getToolsClient client)
+        upload-history (if (nil? history-id)
+                         (:id (histories/get-current-history client))
+                         history-id)]
+    (.create tool-client history-id
              (ToolInputs. tool-id clean-params))))
 
 (defn upload-to-history
-  "Upload a file to the current Galaxy history via URL."
-  [client file-url dbkey ftype]
+  "Upload a file via URL to a Galaxy history, defaulting to the current."
+  [client file-url dbkey ftype & {:keys [history-id]}]
   (run-tool client "upload1"
             {:file-type (name ftype)
              :dbkey (name dbkey)
              "files_0|url_paste" file-url
-             "files_0|NAME" (last (string/split file-url #"/"))}))
+             "files_0|NAME" (last (string/split file-url #"/"))}
+            :history-id history-id))
